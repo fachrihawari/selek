54e126dc890f0ccefe4ea37db3ae2f18fc82b3b4
Fachri Hawari <fachri.hawari@gmail.com>
2025-08-06 22:03:29 +0700

    feat(docker): update Dockerfiles for client and server to use bun 1.2.19 and improve build process

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,6 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig((config) => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+
+
+  // Enforce tp use node version of react-dom/server
+  resolve: config.command === "build"
+    ? {
+      alias: {
+        "react-dom/server": "react-dom/server.node",
+      },
+    }
+    : undefined,
+}));
